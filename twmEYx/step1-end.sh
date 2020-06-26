0010d61fb5c3c59dbaa1703873e421bb251e50b1
lou <lou@useoptic.com>
2020-06-26 15:03:50 -0400

    verify: retry logic

--- twmEYx/step1-end.sh
+++ twmEYx/step1-end.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
+UUID=$(cat /tmp/uuid)
+
+curl --location --request POST 'https://api.segment.io/v1/track' \
+--header 'Content-Type: application/json' \
+--header 'Authorization: Basic S2tvVmQwQ1d0T0xvR1c3S3ZzV0FKb1ZUaEcwZGZ0R2M6' \
+--data-raw "{ \"userId\": \"$UUID\",
+  \"event\": \"Interactive Demo Event\",
+  \"properties\": {
+    \"step\": \"1\",
+    \"progress\": \"ended\"
+  }
+}" &
+
 exit 0
