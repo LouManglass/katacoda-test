a46f7899302380447ce3558c09514b2e41999225
lou <lou@useoptic.com>
2020-07-10 14:15:07 -0400

    containerised: docker run in bg

--- VGeWrF/init.sh
+++ VGeWrF/init.sh
@@ -2,7 +2,7 @@
 
 docker pull opticlou/optic-demo:latest
 
-docker run -p 4000:4000 -p 34444:34444 opticlou/optic-demo
+docker run -p 4000:4000 -p 34444:34444 opticlou/optic-demo &
 
 curl -XGET http://localhost:4000/
 
