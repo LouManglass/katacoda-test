0b2e7589868126698ca5da8e77727318edd9541b
lou <lou@useoptic.com>
2020-07-10 11:04:14 -0400

    containerised: test requests added

--- VGeWrF/init.sh
+++ VGeWrF/init.sh
@@ -3,3 +3,7 @@
 docker pull opticlou/optic-demo:latest
 
 docker run -p 4000:4000 -p 34444:34444 opticlou/optic-demo
+
+curl -XGET http://localhost:4000/posts
+curl -XGET http://localhost:4000/comments
+curl -XGET http://localhost:4000/posts/1
