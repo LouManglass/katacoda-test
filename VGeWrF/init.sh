f1bc2700856a70d893302f5800d6eae04eb3059a
lou <lou@useoptic.com>
2020-07-10 12:12:32 -0400

    containerised: fixed sleep logic

--- VGeWrF/init.sh
+++ VGeWrF/init.sh
@@ -4,11 +4,15 @@ docker pull opticlou/optic-demo:latest
 
 docker run -p 4000:4000 -p 34444:34444 opticlou/optic-demo
 
-while ! curl -XGET http://localhost:4000/
+curl -XGET http://localhost:4000/
+
+while test $? -gt 0 
 do
     echo "Waiting..."
     sleep 3
+    curl -XGET http://localhost:4000/
 done
+
 curl -XGET http://localhost:4000/posts
 curl -XGET http://localhost:4000/comments
 curl -XGET http://localhost:4000/posts/1
