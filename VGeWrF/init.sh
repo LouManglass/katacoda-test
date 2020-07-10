7c60f05d0e9ab88173b5e0e6395269e0c9d15157
lou <lou@useoptic.com>
2020-07-10 11:27:18 -0400

    modified init

--- VGeWrF/init.sh
+++ VGeWrF/init.sh
@@ -4,6 +4,11 @@ docker pull opticlou/optic-demo:latest
 
 docker run -p 4000:4000 -p 34444:34444 opticlou/optic-demo
 
+while ! curl -XGET http://localhost:4000/
+do
+    echo "Waiting..."
+    sleep 3
+done
 curl -XGET http://localhost:4000/posts
 curl -XGET http://localhost:4000/comments
 curl -XGET http://localhost:4000/posts/1
